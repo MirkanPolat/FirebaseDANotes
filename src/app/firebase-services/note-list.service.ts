b1c4a9e0dc00e8ee5e1d8466ce8600fcb0dd9472
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-15 23:32:58 +0200

    Enhance NoteListService with Firestore snapshot listeners for real-time notes updates

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,28 +1,48 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { collectionData, Firestore, collection, doc } from '@angular/fire/firestore';
+import { collectionData, Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NoteListService {
 
+export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
   
   notes$;
   firestore: Firestore = inject(Firestore);
 
+  unsubList; 
+  unsubSingle;
+
   constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+
+    this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "a665s4d6a1s9d"), (element) => {
+    });
+
+    this.unsubSingle();
+    this.unsubList();
+
     this.notes$ = collectionData(this.getNotesRef());
-   }
+    this.notes$ = this.notes$.subscribe( (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+  }
 
   getNotesRef(){
     return collection(this.firestore, 'notes');
   }
 
-   getTrashRef(){
+  getTrashRef(){
     return collection(this.firestore, 'trash');
   }
 

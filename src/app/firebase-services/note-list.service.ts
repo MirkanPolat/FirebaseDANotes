fe239f3be03b47f231efb716239b28749a73fff0
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-15 18:04:36 +0200

    Refactor NoteListService to include Firestore integration and methods for notes management

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+  
+  firestore: Firestore = inject(Firestore);
+
   constructor() { }
+
+ /*  const itemsCollection = collection(this.firestore, 'notes'); */
+
+  getNotesRef(){
+    return collection(this.firestore, 'notes');
+  }
+
+   getTrashRef(){
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colID: string, docID: string){
+    return doc(collection(this.firestore, colID), docID);
+  }
 }
